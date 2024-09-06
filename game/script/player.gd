878817c96c4f59be74b87420fc5bcc1133bfaf11
xz <xz285616@163.com>
2024-09-06 15:26:34 +0800

    project init

---
+++ game/script/player.gd
@@ -0,0 +1,33 @@
+extends CharacterBody3D
+
+
+const SPEED = 10
+const JUMP_VELOCITY = 22
+
+
+func _physics_process(delta: float) -> void:
+	# 左右方向
+	if velocity.x != 0:
+		var faceRight = velocity.x > 0
+		if faceRight:
+			$Visual.rotation = Vector3(0,PI/2,0)
+		else:
+			$Visual.rotation = Vector3(0,-PI/2,0)
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta *8
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, 1)
+
+	move_and_slide()
